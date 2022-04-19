d4568311b85ec38bde1899c84da05d5ecd15f42b
Sebastian Rodriguez <me@srod.ca>
2022-04-18 23:15:50 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,25 +1,37 @@
 import DiscordJS, { Intents } from 'discord.js'
 import WOKCommands from 'wokcommands'
 import path from 'path'
+// import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const client = new DiscordJS.Client({
-  // These intents are recommended for the built in help menu
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  ],
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        // Intents.FLAGS.GUILD_PRESENCES
+    ],
 })
 
-client.on('ready', () => {
-  new WOKCommands(client, {
-    // The name of the local folder for your command files
-    commandsDir: path.join(__dirname, 'commands'),
-    // IN DEV Allow importing of .ts files if you are using ts-node
-    typeScript: true,
-  })
+client.on('ready', async () => {
+    // Alternative Mongo db connection 
+    // await mongoose.connect(process.env.MONGO_URI || '', {
+    //     keepAlive: true
+    // })
+
+    new WOKCommands(client, {
+        // mongoUri: process.env.MONGO_URI,
+        commandsDir: path.join(__dirname, 'commands'),
+        testServers: ['955569135893508207'],
+        botOwners:[
+            '551642556027371523',
+            '765712592165404702'
+        ],
+
+        // IN DEV Allow importing of .ts files if you are using ts-node
+        // typeScript: true,
+    })
 })
 
 client.login(process.env.TOKEN)
